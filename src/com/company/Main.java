e104837cc29e44651de11bf65c48047eb54bd5ae
lucas <lucasgauvain@gmail.com>
2020-09-20 08:49:24 +0200

    Correction de l'exo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,31 +5,37 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner( System.in );
 
-        System.out.println("Veuillez saisir un nombre : ");
-        int userNumberInput =Integer.parseInt(sc.next());
+        int result;
+        try {
+            Scanner sc = new Scanner( System.in );
+
+            System.out.println("Veuillez saisir un nombre : ");
+            int userNumberInput =Integer.parseInt(sc.next());
 
 
+            if (userNumberInput > 0){
+                result = userNumberInput * 2;
 
-        if (userNumberInput > 0){
-            userNumberInput *= 2;
-            System.out.println("Votre nombre multiplié par 2 = " + userNumberInput);
+            } else if (userNumberInput == 0){
+                System.out.println("Veuillez saisir votre nom : ");
+                String userNameInput = sc.next();
+                result = userNameInput.length();
+                if (result > 7){
+                    System.out.println("Que ton nom est long");
+                } else {
+                    System.out.println("Que ton nom est court");
+                }
 
-        } else if (userNumberInput == 0){
-            System.out.println("Veuillez saisir votre nom : ");
-            String userNameInput = sc.next();
-            if (userNameInput.length() > 7){
-                System.out.println("Que ton nom est long");
             } else {
-                System.out.println("Que ton nom est court");
+                result = userNumberInput / 2;
+
             }
-
-        } else {
-            userNumberInput /= 2;
-            System.out.println("Votre nombre divisé par 2 = " + userNumberInput);
-
+        } catch (NumberFormatException e) {
+            System.out.println("Désolé, votre n'est pas valide, je ne peux rien faire");
+            result = -1;
         }
+        System.out.println("Le résultat du calcul = " + result);
         System.out.println("Au revoir");
     }
 }
